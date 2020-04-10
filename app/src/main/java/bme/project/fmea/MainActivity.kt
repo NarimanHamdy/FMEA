de59d1049fd132f1b513350d99491df3b51d9b5f
Mohamed Hamdy <mohamed.hamdyeg98@gmail.com>
2020-04-10 16:09:40 +0200

    :tada: Initial Commit

---
+++ app/src/main/java/bme/project/fmea/MainActivity.kt
@@ -0,0 +1,12 @@
+package bme.project.fmea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
