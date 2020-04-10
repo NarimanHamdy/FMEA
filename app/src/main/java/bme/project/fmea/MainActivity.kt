fd2581e8478e01a3c0e154fcfe51601c1770ae43
Mohamed Hamdy <mohamed.hamdyeg98@gmail.com>
2020-04-10 17:42:25 +0200

    :lipstick: Add Device UI blueprints

--- app/src/main/java/bme/project/fmea/MainActivity.kt
+++ app/src/main/java/bme/project/fmea/MainActivity.kt
@@ -1,12 +1,21 @@
 package bme.project.fmea
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import bme.project.fmea.adddevice.AddDeviceActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fabAdd.setOnClickListener {
+            if (bottomNav.selectedItemId == R.id.nav_devices){
+                startActivity(Intent(this,AddDeviceActivity::class.java))
+            }
+        }
     }
 }
---
+++ app/src/main/java/bme/project/fmea/adddevice/AddDeviceActivity.kt
@@ -0,0 +1,14 @@
+package bme.project.fmea.adddevice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bme.project.fmea.R
+
+class AddDeviceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_device)
+
+    }
+}
